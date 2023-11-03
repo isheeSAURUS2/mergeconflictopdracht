5db28afe8be80e1c17b2f2ea4b18cb4364f054c1
Yunus <38208@ma-web.nl>
2023-11-03 14:36:41 +0100

    UwU3

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function basicHaiku()
 {
     return ["Toward those short trees","We saw a batman descending","On a day in spring."]
 }
-//zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
+//zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen   rerewfdfsdfs
 
 haikus = [
     basicHaiku()
